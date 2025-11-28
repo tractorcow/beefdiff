0a7c8a85cbf67ed7cad658b311e1fae60b9a32ad
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2025-11-28 16:34:53 +1300

    feat: switch to pnpm, update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "prettier/prettier": "error",
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "coverage/", "*.js", "*.mjs"],
+  }
+);
